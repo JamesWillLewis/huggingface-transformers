69e1bbffe869806838782f700cda05a6c2554be4
James Lewis <james.will.lewis@gmail.com>
2024-07-04 00:46:37 -0400

    Add some basic inference

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from transformers import pipeline
+
+# use apple metal
+DEVICE = 'mps'
+MODEL = 'distilbert/distilbert-base-uncased-finetuned-sst-2-english'
 def main():
-    print("hello world")
+    classifier = pipeline("sentiment-analysis", model=MODEL, device=DEVICE)
+    print(classifier("We are very happy to show you the 🤗 Transformers library."))
 
 
 if __name__ == "__main__":
